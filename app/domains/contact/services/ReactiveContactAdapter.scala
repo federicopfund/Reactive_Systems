dd2461bb1e884600a87b762ab34c167d8927a736
Federico Christian Pfund <74926730+federicopfund@users.noreply.github.com>
2026-05-11 04:33:59 +0000

    feat(domains/contact): add ReactiveContactAdapter domain component

---
+++ app/domains/contact/services/ReactiveContactAdapter.scala
@@ -0,0 +1,21 @@
+package domains.contact.services
+
+import akka.actor.typed.{ActorRef, Scheduler}
+import akka.actor.typed.scaladsl.AskPattern._
+import akka.util.Timeout
+import domains.contact.engines._
+import infrastructure.guardian._
+
+import javax.inject._
+import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration._
+
+@Singleton
+class ReactiveContactAdapter @Inject()(
+  guardian: ActorRef[DomainGuardianCommand],
+  implicit val scheduler: Scheduler
+)(implicit ec: ExecutionContext) {
+  private implicit val timeout: Timeout = 5.seconds
+  def submitContact(contact: Contact): Future[ContactResponse] =
+    guardian.ask[ContactResponse](replyTo => ForwardContact(SubmitContact(contact, replyTo)))
+}
