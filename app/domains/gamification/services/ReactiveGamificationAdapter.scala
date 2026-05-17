aaf627f331475c0acb542ef488f471b398ec3310
Federico Christian Pfund <74926730+federicopfund@users.noreply.github.com>
2026-05-17 23:28:30 +0000

    fix(gamification-adapter): añadir import engines.gamification._ para acceso al protocolo GamificationEngine
    GamificationCommand, GamificationResponse y subtypes ahora viven en el sub-package.
    El adapter delega CheckBadges y AwardBadge al actor vía ask pattern.

--- app/domains/gamification/services/ReactiveGamificationAdapter.scala
+++ app/domains/gamification/services/ReactiveGamificationAdapter.scala
@@ -4,6 +4,7 @@ import akka.actor.typed.{ActorRef, Scheduler}
 import akka.actor.typed.scaladsl.AskPattern._
 import akka.util.Timeout
 import domains.gamification.engines._
+import domains.gamification.engines.gamification._
 import infrastructure.guardian._
 
 import javax.inject._
